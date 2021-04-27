820a20915debd00fc9c52b605ded3ff72cf4b0cb
critbase <12738442+critbase@users.noreply.github.com>
2021-04-27 18:45:30 -0400

    i'm not a professional

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
 
             src = fetchurl {
               url =
-                "https://dev.funkwhale.audio/funkwhale/funkwhale/-/archive/${super.version}/funkwhale-${super.version}.tar.bz2";
+                "https://dev.funkwhale.audio/funkwhale/funkwhale/-/archive/${self.version}/funkwhale-${self.version}.tar.bz2";
               sha256 = lib.fakeSha256;
             };
 
