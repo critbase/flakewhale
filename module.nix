5e6ac6b3ece8387b0ce37b70af24161178c4a49c
critbase <12738442+critbase@users.noreply.github.com>
2021-04-28 01:12:29 -0400

    add some more nginx

--- module.nix
+++ module.nix
@@ -310,9 +310,17 @@ in {
       recommendedGzipSettings = true;
       virtualHosts = let
         proxyConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
           proxy_set_header X-Forwarded-Host $host:$server_port;
           proxy_set_header X-Forwarded-Port $server_port;
           proxy_redirect off;
+
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $connection_upgrade;
         '';
         withSSL = cfg.protocol == "https";
       in {
